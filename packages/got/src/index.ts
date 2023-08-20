b1ecc3ceca4d1ac9a8328294104103bb6a3a6efb
Jane Jeon <git@janejeon.com>
2023-08-20 02:24:36 +0100

    feat(got): new package yay

---
+++ packages/got/src/index.ts
@@ -0,0 +1,8 @@
+import { gotSsrf } from 'got-ssrf'
+import { gotScraping } from 'got-scraping'
+
+import type { Got } from 'got'
+
+// The `stream` types are incompatible because got-scraping is using their own build of got...
+// Yay...
+export default gotSsrf.extend(gotScraping as unknown as Got)
